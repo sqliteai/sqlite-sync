c46377fc2c70007d5b9e57224b5ab84f123057de
Gioele Cantoni <gioele.cantoni@gmail.com>
2025-06-12 12:23:50 +0200

    test: lower peers count

--- test/main.c
+++ test/main.c
@@ -16,7 +16,7 @@
 #include <pthread.h>
 #endif
 
-#define PEERS           50
+#define PEERS           5
 #define DB_PATH         "health-track.sqlite"
 #define EXT_PATH        "./dist/cloudsync"
 #define RCHECK          if (rc != SQLITE_OK) goto abort_test;
