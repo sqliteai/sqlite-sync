0113cf503ae675eae879c3b86f4bcd1dda21b146
Gioele Cantoni <gioele.cantoni@gmail.com>
2025-06-12 13:43:14 +0200

    test 2 peer

--- test/main.c
+++ test/main.c
@@ -16,7 +16,7 @@
 #include <pthread.h>
 #endif
 
-#define PEERS           1
+#define PEERS           2
 #define DB_PATH         "health-track.sqlite"
 #define EXT_PATH        "./dist/cloudsync"
 #define RCHECK          if (rc != SQLITE_OK) goto abort_test;
