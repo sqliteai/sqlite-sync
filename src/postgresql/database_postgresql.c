13367c1e8958b067351dd2b990c8b0e25669021f
Marco Bambini <marco@creolabs.com>
2026-01-06 11:50:15 +0100

    Update database_postgresql.c

--- src/postgresql/database_postgresql.c
+++ src/postgresql/database_postgresql.c
@@ -244,18 +244,16 @@ static int map_spi_result (int rc) {
     }
 }
 
-static void clear_fetch_batch(pg_stmt_t *stmt) {
+static void clear_fetch_batch (pg_stmt_t *stmt) {
     if (!stmt) return;
-    if (stmt->last_tuptable) {
-        SPI_freetuptable(stmt->last_tuptable);
-        stmt->last_tuptable = NULL;
-    }
+    if (stmt->row_mcxt) MemoryContextReset(stmt->row_mcxt);
+    
     stmt->current_tuple = NULL;
     stmt->current_tupdesc = NULL;
-    if (stmt->row_mcxt) MemoryContextReset(stmt->row_mcxt);
+    stmt->last_tuptable = NULL;
 }
 
-static void close_portal(pg_stmt_t *stmt) {
+static void close_portal (pg_stmt_t *stmt) {
     if (!stmt) return;
     if (stmt->portal) {
         SPI_cursor_close(stmt->portal);
