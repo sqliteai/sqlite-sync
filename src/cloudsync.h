c619839cc33bf8329a7e1c00c867470dc355a9b3
Andrea Donetti <andinux@gmail.com>
2026-04-08 15:24:38 -0600

    chore: bump version

--- src/cloudsync.h
+++ src/cloudsync.h
@@ -18,7 +18,7 @@
 extern "C" {
 #endif
 
-#define CLOUDSYNC_VERSION                       "1.0.8"
+#define CLOUDSYNC_VERSION                       "1.0.10"
 #define CLOUDSYNC_MAX_TABLENAME_LEN             512
 
 #define CLOUDSYNC_VALUE_NOTSET                  -1
